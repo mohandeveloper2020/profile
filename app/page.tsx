e111fa08a8b601d6adf88c8fbd0bee1f909e7a1f
unknown <mohandeveloper2020@gmail.com>
2026-04-24 22:52:56 +0530

    vision statement showing

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <SkillsSection />
       <ExperienceSection />
       <ProjectsSection />
-      {/* <VisionSection /> */}
+      <VisionSection />
     </div>
   );
 }
